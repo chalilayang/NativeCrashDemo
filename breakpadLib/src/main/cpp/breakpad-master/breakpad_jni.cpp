87a640cd5323b458d97f3ebee86ff4de98024778
chalilayang <chalilayang@sohu-inc.com>
2018-03-18 15:08:02 +0800

    新源码

---
+++ breakpadLib/src/main/cpp/breakpad-master/breakpad_jni.cpp
@@ -0,0 +1,53 @@
+#include <jni.h>
+#include "client/linux/handler/exception_handler.h"
+#include "client/linux/handler/minidump_descriptor.h"
+#include "../customlog.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void Crash() {
+    volatile int* a = reinterpret_cast<volatile int*>(NULL);
+    *a = 1;
+}
+
+bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+                  void* context,
+                  bool succeeded) {
+    LOGP("Dump path: %s\n", descriptor.path());
+    if (succeeded) {
+        LOG("Dump path: success\n");
+    } else {
+        LOG("Dump path: failed\n");
+    }
+    return succeeded;
+}
+
+JNIEXPORT jint
+JNICALL
+Java_com_breakpad_BreakpadWrapper_createCrash(JNIEnv *env, jobject thiz) {
+    LOG("createCrash");
+    Crash();
+    return 0;
+}
+
+google_breakpad::MinidumpDescriptor * descriptor;
+google_breakpad::ExceptionHandler * eh;
+
+JNIEXPORT jint
+JNICALL
+Java_com_breakpad_BreakpadWrapper_initBreakpad(JNIEnv *env, jobject thiz) {
+    LOG("initBreakpad");
+    if (descriptor == nullptr) {
+        descriptor = new google_breakpad::MinidumpDescriptor("./breakpad");
+    }
+    if (eh == nullptr) {
+        eh = new google_breakpad::ExceptionHandler(*descriptor, NULL, DumpCallback, NULL, true, -1);
+    }
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
